ec918f997ba54d7bd37ada20f0a72db7f2c9b317
yodaum <yod.aum@gmail.com>
2023-03-06 03:51:27 +0000

    Complete Array Average Lab

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double totSum = 0.0;
+        for (int i = 0; i < nums.length; i++){
+            totSum = totSum + nums[i];
+        }
+        return totSum/nums.length;
     }
 }
